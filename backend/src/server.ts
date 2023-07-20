1e0408dbe341e581b3b4c070ecd5f10dd60d031d
Rick Huang <rick.liyue.huang@gmail.com>
2023-07-20 12:47:00 +1000

    ci: import mongodb and create initial server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,28 @@
+import 'dotenv/config'; // store the local environment variables in the process.env object
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { validateEnv } from './utils/validate-env';
 
 const app = express();
 
-const PORT: number = 4000;
+const PORT = validateEnv.PORT || 4000;
+const MONGO_CONNECTION_URL_STRING = validateEnv.MONGO_CONNECTION_URL_STRING;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// connect the database firstly
+mongoose
+  .connect(MONGO_CONNECTION_URL_STRING)
+  .then(() => {
+    console.log('Connected to the database!');
+
+    // then start the server
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
---
+++ backend/src/utils/validate-env.ts
@@ -0,0 +1,6 @@
+import { cleanEnv, str, port } from 'envalid';
+
+export const validateEnv = cleanEnv(process.env, {
+  MONGO_CONNECTION_URL_STRING: str(),
+  PORT: port(),
+});
